eb41c48584f9cf3a0e556267bb2c59d6110661eb
Muhammad Amjad <perochak@users.noreply.github.com>
2025-04-06 18:07:57 +0500

    updates

--- tts.py
+++ tts.py
@@ -1,12 +1,16 @@
 from TTS.api import TTS
 import torch
 
-# Use .to(device) instead of gpu=True
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Load model and move it to the right device
-tts_model = TTS(model_name="suhaibrashid17/XTTS-v2-Urdu-FT", progress_bar=False)
-tts_model.to(device)
+tts = TTS.from_pretrained("tts_models/multilingual/multi-dataset/xtts_v2", 
+                          speaker_wav=None, 
+                          language="ur", 
+                          use_deepspeed=False)
+
+tts.load_custom_model("suhaibrashid17/XTTS-v2-Urdu-FT")  # <- This is the fine-tuned Urdu model
+
+tts.to(device)
 
 def generate_voice(text, output_path):
-    tts_model.tts_to_file(text=text, file_path=output_path, speaker_wav=None, language="ur")
+    tts.tts_to_file(text=text, file_path=output_path, language="ur")
